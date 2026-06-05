930458e0f385b7982cedf8dfe5709c41cd0cc898
JKrishnaD <jayak5063@gmail.com>
2026-06-05 16:40:25 +0530

    feat: add packets parsing

--- xdp-ebpf/src/main.rs
+++ xdp-ebpf/src/main.rs
@@ -1,8 +1,16 @@
 #![no_std]
 #![no_main]
 
+use core::mem;
+
 use aya_ebpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
 use aya_log_ebpf::info;
+use network_types::{
+    eth::{EthHdr, EtherType},
+    ip::{IpError, IpProto, Ipv4Hdr},
+    tcp::TcpHdr,
+    udp::UdpHdr,
+};
 
 #[xdp]
 pub fn xdp_filter(ctx: XdpContext) -> u32 {
@@ -12,9 +20,55 @@ pub fn xdp_filter(ctx: XdpContext) -> u32 {
     }
 }
 
+#[inline(always)]
+fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, u32> {
+    let start = ctx.data();
+    let end = ctx.data_end();
+    let len = mem::size_of::<T>();
+
+    if start + offset + len > end {
+        return Err(xdp_action::XDP_ABORTED);
+    }
+
+    Ok((start + offset) as *const T)
+}
+
 fn try_xdp_filter(ctx: XdpContext) -> Result<u32, u32> {
     info!(&ctx, "received a packet");
+    // Reciving the Ethernet headers
+    let ethhdr: *const EthHdr = ptr_at::<EthHdr>(&ctx, 0)?;
 
+    // Verifying the Ethernet type is IPv4
+    match unsafe { (*ethhdr).ether_type() } {
+        Ok(EtherType::Ipv4) => {}
+        _ => return Err(xdp_action::XDP_ABORTED),
+    }
+
+    // Verifying the IP protocol is IPv4
+    let ipv4hdr: *const Ipv4Hdr = ptr_at::<Ipv4Hdr>(&ctx, EthHdr::LEN)?;
+
+    // Source address from where the packet was sent
+    let source_addr = u32::from_be_bytes(unsafe { (*ipv4hdr).src_addr });
+    info!(&ctx, "source_addr: {}", source_addr);
+
+    // Next protocol box
+    let proto = unsafe { (*ipv4hdr).proto() }
+        .map_err(|IpError::InvalidProto(_proto)| xdp_action::XDP_ABORTED)?;
+
+    // Source port from where the packet was sent
+    let source_port = match proto {
+        IpProto::Tcp => {
+            let tcp_hdr: *const TcpHdr = ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
+            u16::from_be_bytes(unsafe { (*tcp_hdr).source })
+        }
+        IpProto::Udp => {
+            let upd_hdr: *const UdpHdr = ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
+            u16::from_be_bytes(unsafe { (*upd_hdr).src_port().to_le_bytes() })
+        }
+        _ => return Err(xdp_action::XDP_ABORTED),
+    };
+
+    info!(&ctx, "SRC IP: {:i}, SRC PORT: {}", source_addr, source_port);
     Ok(xdp_action::XDP_PASS)
 }
 
